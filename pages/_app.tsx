c670b64edfd2e8cabaea5421252ac6b90a22baef
z1ayr0n <2infamouz@gmail.com>
2022-05-09 21:04:26 -0400

    3 files modified, 2 added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import { AppProps } from 'next/app';
 
-const App = () => {};
+const App = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
 
 export default App;
